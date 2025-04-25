867c61844c26b73f1f955c8a418cac9b2ecf9598
AlejoFFF5 <alejoff777@gmail.com>
2025-04-25 07:00:59 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+
+<svg class='i i-facebook' viewBox='0 0 24 24'>
+<path d='M17 14h-3v8h-4v-8H7v-4h3V7a5 5 0 0 1 5-5h3v4h-3q-1 0-1 1v3h4Z'/>
+</svg>
+<a href='https://example.com?client=test&amp;mode=dark'>Link</a>
